2a0436ab36f9dac99f8f9523de465fd5429b6f91
Artem <adpm@list.ru>
2023-08-26 05:05:59 +0600

    update DI

--- app/src/main/java/com/example/myhouse/di/DependencyInjection.kt
+++ app/src/main/java/com/example/myhouse/di/DependencyInjection.kt
@@ -2,20 +2,35 @@ package com.example.myhouse.di
 
 import com.example.myhouse.data.repository.CamerasRepositoryImpl
 import com.example.myhouse.data.repository.DoorsRepositoryImpl
+import com.example.myhouse.data.repository.RoomRepositoryImpl
 import com.example.myhouse.data.storage.CamerasStorage
 import com.example.myhouse.data.storage.DoorsStorage
+import com.example.myhouse.data.storage.RoomsStorage
 import com.example.myhouse.data.storage.Storage
 import com.example.myhouse.data.storage.local.doors.DoorsDataSource
 import com.example.myhouse.data.storage.local.ProvideRealm
+import com.example.myhouse.data.storage.local.cameras.CamerasDataSource
+import com.example.myhouse.data.storage.local.cameras.CamerasDataSourceImpl
 import com.example.myhouse.data.storage.local.doors.DoorsDataSourceImpl
+import com.example.myhouse.data.storage.local.rooms.RoomDataSource
+import com.example.myhouse.data.storage.local.rooms.RoomDataSourceImpl
 import com.example.myhouse.data.storage.network.EndpointImpl
 import com.example.myhouse.data.storage.network.NetworkEndpoints
 import com.example.myhouse.data.storage.network.NetworkService
 import com.example.myhouse.domain.repository.CamerasRepository
 import com.example.myhouse.domain.repository.DoorsRepository
+import com.example.myhouse.domain.repository.RoomsRepository
+import com.example.myhouse.domain.usecase.cameras.GetAllCamerasFromDb
 import com.example.myhouse.domain.usecase.cameras.GetAllNetworkCameras
 import com.example.myhouse.domain.usecase.cameras.GetAllNetworkRooms
+import com.example.myhouse.domain.usecase.cameras.InsertCameraToDb
+import com.example.myhouse.domain.usecase.cameras.UpdateCameraNameInDb
+import com.example.myhouse.domain.usecase.doors.GetAllDoorsInDb
 import com.example.myhouse.domain.usecase.doors.GetAllNetworkDoors
+import com.example.myhouse.domain.usecase.doors.InsertDoorsToDb
+import com.example.myhouse.domain.usecase.doors.UpdateDoorsNameInDb
+import com.example.myhouse.domain.usecase.rooms.GetAllRoomFromDb
+import com.example.myhouse.domain.usecase.rooms.InsertRoomToDb
 import com.example.myhouse.presentation.viewmodels.MainViewModel
 import io.ktor.client.HttpClient
 import io.realm.kotlin.Realm
@@ -24,23 +39,37 @@ import org.koin.dsl.module
 
 val DependencyInjection = module {
 
-    //Data
+    //Local
     single<Realm> { ProvideRealm.realm }
     single<DoorsDataSource> { DoorsDataSourceImpl(get()) }
-
+    single<RoomDataSource> { RoomDataSourceImpl(get()) }
+    single<CamerasDataSource> { CamerasDataSourceImpl(get()) }
+    //Network
     single  <HttpClient>{ NetworkService.client }
     single<NetworkEndpoints> { EndpointImpl(get()) }
-
-    single<CamerasStorage> { Storage(get()) }
-    single<DoorsStorage> { Storage(get()) }
-
+    //Storage
+    single<CamerasStorage> { Storage(get(), get(), get(), get()) }
+    single<DoorsStorage> { Storage(get(), get(), get(), get()) }
+    single<RoomsStorage> { Storage(get(), get(), get(), get()) }
+    //RepositoryImpl
     single<CamerasRepository> { CamerasRepositoryImpl(get()) }
     single<DoorsRepository> { DoorsRepositoryImpl(get()) }
+    single<RoomsRepository> { RoomRepositoryImpl(get()) }
 
     //Usecases
-    factory { GetAllNetworkDoors(get()) }
+    factory { GetAllCamerasFromDb(get()) }
     factory { GetAllNetworkCameras(get()) }
     factory { GetAllNetworkRooms(get()) }
+    factory { InsertCameraToDb(get()) }
+    factory { UpdateCameraNameInDb(get()) }
+
+    factory { GetAllDoorsInDb(get()) }
+    factory { GetAllNetworkDoors(get()) }
+    factory { InsertDoorsToDb(get()) }
+    factory { UpdateDoorsNameInDb(get()) }
+
+    factory { GetAllRoomFromDb(get()) }
+    factory { InsertRoomToDb(get()) }
 
 
     // View Model
@@ -49,7 +78,14 @@ val DependencyInjection = module {
             get(),
             get(),
             get(),
-
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
         )
     }
 
--- app/src/main/java/com/example/myhouse/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/myhouse/presentation/viewmodels/MainViewModel.kt
@@ -3,15 +3,31 @@ package com.example.myhouse.presentation.viewmodels
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myhouse.domain.usecase.cameras.GetAllCamerasFromDb
 import com.example.myhouse.domain.usecase.cameras.GetAllNetworkCameras
 import com.example.myhouse.domain.usecase.cameras.GetAllNetworkRooms
+import com.example.myhouse.domain.usecase.cameras.InsertCameraToDb
+import com.example.myhouse.domain.usecase.cameras.UpdateCameraNameInDb
+import com.example.myhouse.domain.usecase.doors.GetAllDoorsInDb
 import com.example.myhouse.domain.usecase.doors.GetAllNetworkDoors
+import com.example.myhouse.domain.usecase.doors.InsertDoorsToDb
+import com.example.myhouse.domain.usecase.doors.UpdateDoorsNameInDb
+import com.example.myhouse.domain.usecase.rooms.GetAllRoomFromDb
+import com.example.myhouse.domain.usecase.rooms.InsertRoomToDb
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val getAllNetworkCameras: GetAllNetworkCameras,
     private val getAllNetworkRooms: GetAllNetworkRooms,
-    private val getAllNetworkDoors: GetAllNetworkDoors
+    private val getAllCamerasFromDb: GetAllCamerasFromDb,
+    private val insertCameraToDb: InsertCameraToDb,
+    private val updateCameraNameInDb: UpdateCameraNameInDb,
+    private val getAllNetworkDoors: GetAllNetworkDoors,
+    private val getAllDoorsInDb: GetAllDoorsInDb,
+    private val insertDoorsToDb: InsertDoorsToDb,
+    private val updateDoorsNameInDb: UpdateDoorsNameInDb,
+    private val getAllRoomFromDb: GetAllRoomFromDb,
+    private val insertRoomToDb: InsertRoomToDb
 ) : ViewModel() {
 
     private val TAG = "TAG-VM"
